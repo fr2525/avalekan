aceb56875c90db6e4f50e76125d61d41a08bc25b
Fabio Reinert <fr2525@gmail.com>
2024-04-06 10:57:53 -0300

    Segundo commit

---
+++ src/main/java/com/ekan/avalekan/resources/CategoriaResource.java
@@ -0,0 +1,31 @@
+package com.ekan.avalekan.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.freinert.cursomc.domain.Categoria;
+import com.freinert.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
+		
+		}
+
+}
